8908ae759d64d6eeb233340d12061661907a43af
Sean Davis <sedavis@sedavis-a01.vmware.com>
2021-01-15 23:06:56 -0500

    quick update

--- formatDocker.sh
+++ formatDocker.sh
@@ -1,4 +1,4 @@
-# Formats docker output to pretty format
-# This shell will only work on bash
+# Formats docker output to pretty format in bash
+# Usage: "source <path/to/formatDocker.sh">
 export FORMAT="\nID\t{{.ID}}\nIMAGE\t{{.Image}}\nCOMMAND\t{{.Command}}\nCREATED\t{{.RunningFor}}\nSTATUS\t{{.Status}}\nPORTS\t{{.Ports}}\nNAMES\t{{.Names}}\n"
 
