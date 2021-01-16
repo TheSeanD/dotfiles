6975f526c7842178d2e25d07f2598b954086c60c
Sean Davis <sedavis@sedavis-a01.vmware.com>
2021-01-15 23:26:53 -0500

    updated docker ps

--- formatDockerPs.sh
+++ formatDockerPs.sh
@@ -1,4 +1,5 @@
-# Formats docker output to pretty format in bash
-# Usage: "source <path/to/formatDocker.sh">
+# Formats docker process status output to pretty format in bash
+# Usage: "docker ps --format $FORMAT"
 export FORMAT="\nID\t{{.ID}}\nIMAGE\t{{.Image}}\nCOMMAND\t{{.Command}}\nCREATED\t{{.RunningFor}}\nSTATUS\t{{.Status}}\nPORTS\t{{.Ports}}\nNAMES\t{{.Names}}\n"
 
+# Dont forget to source this file :)
